eb8928c6d3e47795d7a5ce3269c052c06aa2a70e
kurtyan <kurtyan777@gmail.com>
2016-02-26 04:30:30 +0800

    added gitignore for compiled target

---
+++ src/main/java/com/github/kurtyan/yunpian4j/util/SimpleHttpClient.java
@@ -0,0 +1,7 @@
+package com.github.kurtyan.yunpian4j.util;
+
+/**
+ * Created by yanke on 16-2-26.
+ */
+public class SimpleHttpClient {
+}
---
+++ src/test/java/com/github/kurtyan/yunpian4j/util/SimpleHttpClientTest.java
@@ -0,0 +1,10 @@
+package com.github.kurtyan.yunpian4j.util;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kurtyan on 16-2-26.
+ */
+public class SimpleHttpClientTest {
+
+}
